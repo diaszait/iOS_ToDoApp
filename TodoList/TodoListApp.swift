27e01ddc8eba1ff7ba02483d49b18050888f2f30
diaszait <diasabub@gmail.com>
2023-09-06 12:02:45 +0600

    ToDoApp done

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Dias Zait on 16.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
